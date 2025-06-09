a1186a8ddce1a0d868120f43e7c5c308dfb42191
saulo <saulobrustolin@icloud.com>
2025-06-09 12:45:16 -0300

    update

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   standalone: false,
@@ -7,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.page.scss'],
 })
 export class ContactPage implements OnInit {
+  contactForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]],
+    });
   }
 
+  submitForm() {
+    if (this.contactForm.valid) {
+      this.presentToast('Formulário enviado com sucesso!', 'success');
+
+      this.contactForm.reset();
+    } else {
+      console.error('Formulário inválido!');
+      this.presentToast('Por favor, preencha todos os campos corretamente.', 'danger');
+    }
+  }
+
+  async presentToast(message: string, color: 'success' | 'danger' | 'warning') {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      position: 'bottom',
+      color: color,
+      buttons: [
+        {
+          text: 'Fechar',
+          role: 'cancel',
+        }
+      ]
+    });
+    await toast.present();
+  }
 }
--- src/app/projects/projects.page.ts
+++ src/app/projects/projects.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ProjectDetailComponent } from '../components/project-detail/project-detail.component';
 
 @Component({
   standalone: false,
@@ -7,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.page.scss'],
 })
 export class ProjectsPage implements OnInit {
+  projects: Array<any> = [
+    {
+      title: "PWA ADS UPF",
+      subtitle: "PWA usando HTML, JS e CSS.",
+      description: "Link do projeto: https://github.com"
+    },
+    {
+      title: "TODO List",
+      subtitle: "App de tarefas usando Ionic.",
+      description: "Link do projeto: https://github.com"
+    },
+    {
+      title: "Personal APP",
+      subtitle: "App de apresentação pessoal com ionic.",
+      description: "Link do projeto: https://github.com"
+    }
+  ]
 
-  constructor() { }
+  constructor(
+    private modalController: ModalController
+  ) { }
 
   ngOnInit() {
+
+  }
+
+  async viewDetails(e: any) {
+    const modal = await this.modalController.create({
+      component: ProjectDetailComponent
+    })
+    await modal.present()
+    const mod = await modal.onDidDismiss()
+    if (mod) {
+      console.log(mod)
+    }
   }
 
 }
